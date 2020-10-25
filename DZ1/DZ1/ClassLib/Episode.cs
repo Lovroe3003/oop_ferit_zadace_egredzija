caf43d9bd32482da7ad63316b1af5e1bf277f978
Lovroe3003 <lovro.egredzija@gmail.com>
2020-10-25 17:55:09 +0100

    Added 1st homework. Working as intended.

---
+++ DZ1/DZ1/ClassLib/Episode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassLib
+{
+    public class Episode
+    {
+        private int Viewers { get; set; }
+        private double TotalScore { get; set; }
+        private double MaxScore { get; set; }
+        
+        public Episode()
+        {
+
+        }
+
+        public Episode(int views, double totalSc, double maxSc)
+        {
+            Viewers = views;
+            TotalScore = totalSc;
+            MaxScore = maxSc;
+        }
+        
+        public void AddView(double rndScore)
+        {
+            Viewers++;
+            TotalScore = TotalScore + rndScore;
+            if (MaxScore < rndScore)
+            {
+                MaxScore = rndScore;
+            }
+        }
+
+        public double GetMaxScore()
+        {
+            return MaxScore;
+        }
+
+        public double GetAverageScore()
+        {
+            double avg = TotalScore / Viewers;
+            return avg;
+        }
+
+        public int GetViewerCount()
+        {
+            return Viewers;
+        }
+        
+    }
+}
